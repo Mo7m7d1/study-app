c965019b26b0974f254da9afc1d2c5cadcc744f3
mo7m7d1 <mo7m7d1@gmail.com>
2023-06-04 21:04:10 +0300

    fix issue

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,11 +5,11 @@ import { useMemo } from "react";
 const BooksPage = () => {
 	const books = useMemo(() => jsonBooks, []);
 	return (
-		<div className="text-white flex flex-col items-center mt-14 lg:mt-28 mx-4">
+		<div className="text-white flex flex-col items-center mt-14 lg:mt-28 mx-4 lg:mx-32">
 			<h1 className="text-4xl font-semibold mb-8 text-gray-400">
 				قائمة المقررات
 			</h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-10 w-full">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 pb-10 w-full">
 				{books.map((book) => (
 					<BookCard key={book.id} title={book.title} link={book.link} />
 				))}
--- src/components/SubjectCard.tsx
+++ src/components/SubjectCard.tsx
@@ -31,7 +31,6 @@ const SubjectCard = ({ id, subject, link, btnText }: SubjectCardType) => {
 						<option value={15}>15</option>
 						<option value={30}>30</option>
 						<option value={50}>50</option>
-						<option value={100}>100</option>
 					</select>
 				)}
 			</div>
